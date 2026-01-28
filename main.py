36539fdd7a962bf0a9c07757c006918d299e339f
Bryce <bwang56@calpoly.edu>
2026-01-28 00:15:46 -0800

    did some more stuff

--- main.py
+++ main.py
@@ -28,42 +28,101 @@ D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24
 855E6EEB 22B3B2E5
 """ # smaller than q and a primitive root of q
 
-    q = int("".join(qhex.split()), 16)
+    q = int("".join(qhex.split()), 16) # make those giant hex values into ints
     a = int("".join(ahex.split()), 16)
 
-    X1 = random.randint(0, q)
-    X2 = random.randint(0, q)
+    X1 = random.randint(1, q - 1) # choose individual secret values (Private Key)
+    X2 = random.randint(1, q - 1)
 
-    Y1 = pow(a, X1, q)
+    Y1 = pow(a, X1, q) # use secret values to generate Public Key
     Y2 = pow(a, X2, q)
 
-    s1 = pow(Y2, X1, q)
+    s1 = pow(Y2, X1, q) # use exchanged public keys to generate shared secret key
     s2 = pow(Y1, X2, q)
 
-    k1 = SHA256.new()
+    k1 = SHA256.new() 
     k2 = SHA256.new()
-    k1.update(s1.to_bytes((s1.bit_length() + 7) // 8, "big"))
+    k1.update(s1.to_bytes((s1.bit_length() + 7) // 8, "big")) # hash the shared secret key with SHA256
     k2.update(s2.to_bytes((s2.bit_length() + 7) // 8, "big"))
 
-    key1 = k1.digest()[:16]
+    key1 = k1.digest()[:16] # shrink the hash digest to 16 bytes because 32 bytes is too long
     key2 = k2.digest()[:16]
 
-    IV = get_random_bytes(16)
+    IV = get_random_bytes(16) # shared initial vector
 
-    text = b"yay this works"
+    text = b"yay this works" # message to encrypt
 
-    c1 = AES.new(key1, AES.MODE_CBC, IV)
-    ciphertext = c1.encrypt(pad(text, 16))
+    c1 = AES.new(key1, AES.MODE_CBC, IV) 
+    ciphertext = c1.encrypt(pad(text, 16)) # use AES CBC to encrypt the text
 
-    print("Alice sends ciphertext:", ciphertext)
+    print("Alice sends ciphertext:", ciphertext) # show the ciphertext just for fun
 
     c2 = AES.new(key2, AES.MODE_CBC, IV)
-    decrypted_text = unpad(c2.decrypt(ciphertext), 16)
+    decrypted_text = unpad(c2.decrypt(ciphertext), 16) # decrypt the ciphertext using AES CBC
 
-    print("Bob received:", decrypted_text)
+    print("Bob received:", decrypted_text) # print the "sent" message
+
+    return
+
+def breached_Diffie_Hellman():
+    """This Diffie Hellman variation includes a man in the middle
+    In a given variable, 1 denotes Alice, 2 denotes Bob
+    """
+    qhex = """ 
+B10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6
+9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0
+13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70
+98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0
+A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708
+DF1FB2BC 2E4A4371
+""" # must be a prime number
+
+    ahex = """
+A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F
+D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213
+160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1
+909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A
+D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24
+855E6EEB 22B3B2E5
+""" # smaller than q and a primitive root of q
+
+    q = int("".join(qhex.split()), 16) # make those giant hex values into ints
+    a = int("".join(ahex.split()), 16)
+
+    X1 = random.randint(1, q - 1) # choose individual secret values (Private Key)
+    X2 = random.randint(1, q - 1) # between 1 and q-1
+
+    Y1 = pow(a, X1, q) # use secret values to generate Public Key
+    Y2 = pow(a, X2, q)
+
+    s1 = pow(Y2, X1, q) # use exchanged public keys to generate shared secret key
+    s2 = pow(Y1, X2, q)
+
+    k1 = SHA256.new() 
+    k2 = SHA256.new()
+    k1.update(s1.to_bytes((s1.bit_length() + 7) // 8, "big")) # hash the shared secret key with SHA256
+    k2.update(s2.to_bytes((s2.bit_length() + 7) // 8, "big"))
+
+    key1 = k1.digest()[:16] # shrink the hash digest to 16 bytes because 32 bytes is too long
+    key2 = k2.digest()[:16]
+
+    IV = get_random_bytes(16) # shared initial vector
+
+    text = b"yay this works" # message to encrypt
+
+    c1 = AES.new(key1, AES.MODE_CBC, IV) 
+    ciphertext = c1.encrypt(pad(text, 16)) # use AES CBC to encrypt the text
+
+    print("Alice sends ciphertext:", ciphertext) # show the ciphertext just for fun
+
+    c2 = AES.new(key2, AES.MODE_CBC, IV)
+    decrypted_text = unpad(c2.decrypt(ciphertext), 16) # decrypt the ciphertext using AES CBC
+
+    print("Bob received:", decrypted_text) # print the "sent" message
 
     return
 
 if __name__ == "__main__":
 
     dh_Key_Ex()
+    breached_Diffie_Hellman()
